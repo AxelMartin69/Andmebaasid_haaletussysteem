83ca892e47e27a05a0367cc8d2af3c212770eb61
AxelMartin69 <75273577+AxelMartin69@users.noreply.github.com>
2022-03-14 19:35:50 +0200

    something works

--- db.php
+++ db.php
@@ -1,14 +1,84 @@
 <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+function voted_persons() {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "haaletus";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    
+    $sql = "SELECT nimi, otsus FROM haaletus /*WHERE otsus != 'haaletamata'*/";
+    $result = $conn->query($sql);
+    
+    if ($result->num_rows > 0) {
+        // output data of each row
+        echo 'Haaletanud inimesed:<br><br>';
+        $cnt = 1;
+        while($row = $result->fetch_assoc()) {
+            echo $cnt.'. '.$row["nimi"].'------'.$row['otsus'];
+            echo "<br>";
+            $cnt++;
+        }
+    } else {
+        echo "Keegi pole haaletanud";
+    }
+    
+    $conn->close();
 }
-echo "Connected successfully";
+
+function update_data($name, $decision) {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "haaletus";
+            
+    $name = strtolower($name);
+            
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    $sql = "UPDATE haaletus SET otsus = '$decision', aeg = CURRENT_TIMESTAMP() WHERE nimi = '$name'";
+            
+    if (mysqli_query($conn, $sql)) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . mysqli_error($conn);
+    }
+    
+    $conn->close();
+    echo "<br>";
+    echo "<br>";
+    voted_persons();
+}
+
+function timer() {
+    
+    // siia on jqueryt vaja
+    // https://phpcoder.tech/how-to-display-current-running-clock-in-php/
+            
+            
+    $min = 0;
+    $sec = 55;
+    echo '0:0 <br>';
+    while ($min != 5) {
+        $sec++;
+        if ($sec <= 9) {
+            $sec = 0 . $sec;
+        } elseif ($sec == 60) {
+            $sec = '00';
+            $min++;
+        };
+        sleep(1);
+        echo $min.':'.$sec.'<br>';
+    };
+};
 ?>
--- index.php
+++ index.php
@@ -9,94 +9,69 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> -->
     </head>
-    <body>
+    <body class="">
         
         
         <?php
+        include 'db.php';
         // define variables and set to empty values
-        $nameErr = $emailErr = $decisionErr = "";
-        $name = $email = $decision = "";
+        $nameErr = $decisionErr = "";
+        $name = $decision = "";
         
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-          if (empty($_POST["name"])) {
-            $nameErr = "Name is required";
-          } else {
-            $name = test_input($_POST["name"]);
-            // check if name only contains letters and whitespace
-            if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-              $nameErr = "Only letters and white space allowed";
+            if (empty($_POST["name"])) {
+                $nameErr = "Name is required";
+            } else {
+                $name = test_input($_POST["name"]);
+                // check if name only contains letters and whitespace
+                if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+                    $nameErr = "Only letters and white space allowed";
+                }
             }
-          }
-        
-          if (empty($_POST["decision"])) {
-            $decisionErr = "Decision is required";
-          } else {
-            $decision = test_input($_POST["decision"]);
-          }
+          
+            if (empty($_POST["decision"])) {
+                $decisionErr = "Decision is required";
+            } else {
+                $decision = test_input($_POST["decision"]);
+            }
         }
         
         function test_input($data) {
-          $data = trim($data);
-          $data = stripslashes($data);
-          $data = htmlspecialchars($data);
-          return $data;
-        }
-        
-        function update_data($name, $decision) {
-            $servername = "193.40.62.9";
-            $username = "tammekandaxelikt";
-            $password = "PSdzxX55nMHx";
-            $dbname = "tammekandaxelikt_haaletus";
-            
-            // Create connection
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            // Check connection
-            if ($conn->connect_error) {
-              die("Connection failed: " . $conn->connect_error);
-            }
-            
-            $sql = "UPDATE HAALETUS SET otsus = '$decision' WHERE nimi = '$name'";
-            
-            if (mysqli_query($conn, $sql)) {
-              echo "Record updated successfully";
-            } else {
-              echo "Error updating record: " . mysqli_error($conn);
-            }
-            
-            $conn->close();
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
         }
         
         if(array_key_exists('Submit', $_POST)) {
             update_data($name, $decision);
-        }
+        };
         
         ?>
         
         <h2>Haaletussysteem</h2>
         <p><span class="error">* required field</span></p>
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-          Name: <input type="text" name="name" value="<?php echo $name;?>">
-          <span class="error">* <?php echo $nameErr;?></span>
-          <br><br>
-          Decision:
-          <input type="radio" name="decision" <?php if (isset($decision) && $decision=="poolt") echo "checked";?> value="poolt">Poolt
-          <input type="radio" name="decision" <?php if (isset($decision) && $decision=="vastu") echo "checked";?> value="vastu">Vastu
-          <span class="error">* <?php echo $decisionErr;?></span>
-          <br><br>
-          <input type="submit" name="submit" value="Submit">  
+            Name: <input type="text" name="name" value="<?php echo $name;?>">
+            <span class="error">* <?php echo $nameErr;?></span>
+            <br><br>
+            Decision:
+            <input type="radio" name="decision" <?php if (isset($decision) &&     $decision=="poolt") echo "checked";?> value="poolt">Poolt
+            <input type="radio" name="decision" <?php if (isset($decision) &&     $decision=="vastu") echo "checked";?> value="vastu">Vastu
+            <input type="radio" name="decision" <?php if (isset($decision) &&     $decision=="erapooletu") echo "checked";?> value="erapooletu">Erapooletu
+            <span class="error">* <?php echo $decisionErr;?></span>
+            <br><br>
+            <input type="submit" name="submit" value="Submit">  
         </form>
         
         <?php
         echo "<h2>Sinu Andmed:</h2>";
         echo $name;
         echo "<br>";
-        echo $email;
-        echo "<br>";
         echo $decision;
-        echo "<br>";
+        echo "<br><br><br>";
         
-        update_data($name, $decision)
-        
+        update_data($name, $decision);
         
         ?>
     </body>
